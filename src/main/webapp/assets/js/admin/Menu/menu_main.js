b89e5945636f5d4fa9bad5b15d5bb2acb38d51bd
choisimo <csimon2k0209@gmail.com>
2023-11-17 20:15:15 +0900

    상세보기, 수정하기 디버그
    404 페이지 해결

--- src/main/webapp/assets/js/admin/Menu/menu_main.js
+++ src/main/webapp/assets/js/admin/Menu/menu_main.js
@@ -44,8 +44,8 @@ function updateMenu(menuType) {
 			    '<p class="card-text" align="center" id="menu_content">' + menu_content + '</p>' +
 			    '<div class="d-flex justify-content-between align-items-center">' +
 			    '<div class="btn-group">' +
-			    '<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath +'admin/Menu/View.jsp?menu_no=' + menu_no + '\')">상세보기</a>' +
-			    '<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath + 'admin/Menu/Edit.jsp?menu_no=' + menu_no + '\')">수정하기</a>' +
+			    '<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath +'Menu/View.jsp?menu_no=' + menu_no + '\')">상세보기</a>' +
+			    '<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath + 'Menu/Edit.jsp?menu_no=' + menu_no + '\')">수정하기</a>' +
 			    '</div>' +
 			    '<small class="text-body-secondary">' + formatNumber(menu_price) + '원</small>' +
 			    '</div>' +
@@ -82,8 +82,8 @@ function updateMenu(menuType) {
 			    '<p class="card-text" align="center" id="menu_content">' + '현재 수량 : ' + component_amount + '</p>' +
 			    '<div class="d-flex justify-content-between align-items-center">' +
 			    '<div class="btn-group">' +
-			   	'<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath +'admin/Menu/componentView.jsp?component_no=' + component_no + '\')">상세보기</a>' +
-			    '<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath + 'admin/Menu/componentEdit.jsp?component_no=' + component_no + '\')">수정하기</a>' +
+			   	'<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath +'Menu/componentView.jsp?component_no=' + component_no + '\')">상세보기</a>' +
+			    '<a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" onclick="openPopup(\'' + contextPath + 'Menu/componentEdit.jsp?component_no=' + component_no + '\')">수정하기</a>' +
 			    '</div>' +
 			    '<small class="text-body-secondary">' + formatNumber(component_price) + '원</small>' +
 			    '</div>' +
